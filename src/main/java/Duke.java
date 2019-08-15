525c7de03fd4161a04b83b7e912bc5e569019350
namiwa <khairul.nasri895@gmail.com>
2019-08-16 01:24:10 +0800

    Duke Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String line = "_______________________________\n";
+    private Scanner scan;
+
+    private Duke() {
+        scan = new Scanner(System.in);
+    }
+
+    private void helloMsg() {
+        System.out.println(
+                line +
+                "Hello! I'm Duke\n" +
+                "What can I do for you?\n" +
+                line);
+    }
+
+    private void loopMsg() {
+        String echo = scan.nextLine();
+        while (!echo.equals("bye")) {
+            System.out.println(line + echo + "\n" + line);
+            echo = scan.nextLine();
+        }
+    }
+
+    private void goodbyeMsg() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private void closeScan() {
+        scan.close();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.helloMsg();
+        duke.loopMsg();
+        duke.goodbyeMsg();
+        duke.closeScan();
     }
 }
