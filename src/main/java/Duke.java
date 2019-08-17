b34e74852292886deefa01e2b15cf540f3c6a9f1
namiwa <khairul.nasri895@gmail.com>
2019-08-17 20:35:46 +0800

    Level 3 Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
-    //TODO: Level 2 fixed list msg + array storage
-    private static String LINE = "_______________________________\n";
     private Scanner scan;
-    private Task task;
+    private List<Task> taskList;
 
 
     private Duke() {
         scan = new Scanner(System.in);
-        task = new Task();
+        taskList = new ArrayList<>();
     }
 
     private void helloMsg() {
+        String LINE = "_______________________________\n";
         System.out.println(
                 LINE +
                 "Hello! I'm Duke\n" +
@@ -20,13 +21,60 @@ public class Duke {
                 LINE);
     }
 
+    private void addedTaskMsg(String task) {
+        System.out.println("added: " + task);
+    }
+
+    private void tryAgainMsg() {
+        System.out.println("Please try again!");
+    }
+
+    private void doneTaskMsg() {
+        System.out.println("Nice! I've marked this task as done:");
+    }
+
+    private Boolean checkValidDoneIndex(String input) throws NumberFormatException {
+        String[] hold = input.split(" ");
+        int test = hold.length;
+        int index = Integer.parseInt(hold[1]);
+        if (test > 2) {
+            return false;
+        } else if(index > taskList.size()) {
+            return false;
+        } else if(index  < 0) {
+            return false;
+        } else {
+            setDoneTask(index);
+            return true;
+        }
+    }
+
+    private void setDoneTask(int index) {
+        taskList.get(index - 1).setTaskDone();
+        doneTaskMsg();
+        System.out.println(taskList.get(index - 1).displayTask());
+    }
+
     private void loopMsg() {
         String input = scan.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                task.displayList();
+                if (taskList.size() == 0) {
+                    tryAgainMsg();
+                }
+                int count = 1;
+                for (Task temp : taskList) {
+                    System.out.println(count + ". " + temp.displayTask());
+                    count++;
+                }
+            } else if (input.startsWith("done")) {
+                Boolean cont = checkValidDoneIndex(input);
+                if (!cont) {
+                    tryAgainMsg();
+                }
             } else {
-                task.addTask(input);
+                taskList.add(new Task(input));
+                addedTaskMsg(input);
             }
             input = scan.nextLine();
         }
@@ -43,7 +91,11 @@ public class Duke {
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.helloMsg();
-        duke.loopMsg();
+        try {
+            duke.loopMsg();
+        } catch (NumberFormatException e) {
+            duke.tryAgainMsg();
+        }
         duke.goodbyeMsg();
         duke.closeScan();
     }
