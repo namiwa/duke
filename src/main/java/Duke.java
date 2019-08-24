7f16f3ae839245791a00942d6940eea6a94033d5
namiwa <khairul.nasri895@gmail.com>
2019-08-24 16:23:29 +0800

    Updated Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,6 @@ public class Duke {
     private Scanner scan;
     private List<Task> taskList;
 
-
     private Duke() {
         scan = new Scanner(System.in);
         taskList = new ArrayList<>();
@@ -25,52 +24,59 @@ public class Duke {
         System.out.println("added: " + task);
     }
 
-    private void tryAgainMsg() {
-        System.out.println("Please try again!");
+    private void tryAgainNumber() {
+        System.out.println("Please input a number!");
+    }
+    private void tryAgainIndex() {
+        System.out.println("Please check your index!");
     }
 
     private void doneTaskMsg() {
         System.out.println("Nice! I've marked this task as done:");
     }
 
-    private Boolean checkValidDoneIndex(String input) throws NumberFormatException {
+    //TODO: change from boolean to void, throws illegal index access later
+    private void checkValidDoneIndex(String input) throws NumberFormatException, IllegalAccessError {
         String[] hold = input.split(" ");
         int test = hold.length;
         int index = Integer.parseInt(hold[1]);
         if (test > 2) {
-            return false;
+            throw new NumberFormatException();
         } else if(index > taskList.size()) {
-            return false;
-        } else if(index  < 0) {
-            return false;
+            throw new NumberFormatException();
+        } else if(index  <= 0) {
+            throw new IllegalAccessError();
         } else {
             setDoneTask(index);
-            return true;
         }
     }
 
     private void setDoneTask(int index) {
         taskList.get(index - 1).setTaskDone();
         doneTaskMsg();
-        System.out.println(taskList.get(index - 1).displayTask());
+        System.out.println(taskList.get(index - 1));
+    }
+
+    private void displayAllTasks() {
+        int count = 1;
+        for (Task temp : taskList) {
+            System.out.println(count + ". " + temp);
+            count++;
+        }
     }
 
     private void loopMsg() {
         String input = scan.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                if (taskList.size() == 0) {
-                    tryAgainMsg();
-                }
-                int count = 1;
-                for (Task temp : taskList) {
-                    System.out.println(count + ". " + temp.displayTask());
-                    count++;
-                }
+                displayAllTasks();
             } else if (input.startsWith("done")) {
-                Boolean cont = checkValidDoneIndex(input);
-                if (!cont) {
-                    tryAgainMsg();
+                try {
+                    checkValidDoneIndex(input);
+                } catch (NumberFormatException e) {
+                    tryAgainNumber();
+                } catch (IllegalAccessError e) {
+                    tryAgainIndex();
                 }
             } else {
                 taskList.add(new Task(input));
@@ -82,6 +88,7 @@ public class Duke {
 
     private void goodbyeMsg() {
         System.out.println("Bye. Hope to see you again soon!");
+        closeScan();
     }
 
     private void closeScan() {
@@ -91,12 +98,7 @@ public class Duke {
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.helloMsg();
-        try {
-            duke.loopMsg();
-        } catch (NumberFormatException e) {
-            duke.tryAgainMsg();
-        }
+        duke.loopMsg();
         duke.goodbyeMsg();
-        duke.closeScan();
     }
 }
