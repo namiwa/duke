9d3ef8fd10ae4e72b9d7bea1b19ab8b81748701c
namiwa <khairul.nasri895@gmail.com>
2019-08-30 21:08:09 +0800

    updated Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,11 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
     private String dateAndTime;
-    private LocalDate date;
+    private LocalDateTime dateTime;
 
     Deadline(String... input) {
         super(input[0]);
@@ -19,7 +21,9 @@ public class Deadline extends Task {
 
     private void setStringToDate(String dateAndTime) {
         String[] split = dateAndTime.split(" ");
-        date = LocalDate.parse(split[0], DateTimeFormatter.ofPattern("d/MM/yyyy"));
+        LocalDate datePart = LocalDate.parse(split[0], DateTimeFormatter.ofPattern("d/MM/yyyy"));
+        LocalTime timePart = LocalTime.parse(split[1], DateTimeFormatter.ofPattern("HHmm"));
+        dateTime =  LocalDateTime.of(datePart, timePart);
     }
 
     public String writingFile() { return "D" + "|" + super.writingFile() + "|" + dateAndTime; }
