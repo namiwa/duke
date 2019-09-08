0b8114533e753cd59c66f0b871eb5a25072a200f
namiwa <khairul.nasri895@gmail.com>
2019-09-08 12:53:20 +0800

    Added JavaDocs for Tasks class

--- src/main/java/Duke/Tasks/Task.java
+++ src/main/java/Duke/Tasks/Task.java
@@ -1,9 +1,12 @@
 package Duke.Tasks;
 
 public class Task {
+    /**
+     * Task is the string value of the task name.
+     * Done is the active status of the task.
+     */
     private String task;
     private Boolean done;
-
 
     public Task(String task) {
         this.task = task;
@@ -18,10 +21,14 @@ public class Task {
         return task;
     }
 
-    public boolean getDone() {
+    private boolean getDone() {
         return done;
     }
 
+    /**
+     * Function to be used to when writing to the file.
+     * @return Returns a string containing task name and done status.
+     */
     public String writingFile() {
         return task + "|" + (getDone() ? "1" : "0");
     }
